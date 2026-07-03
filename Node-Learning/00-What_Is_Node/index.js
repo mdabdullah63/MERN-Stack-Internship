9529d011e2196845233f146a1c0f104844fa63ab
MD Abdullah <mdabdullahrah67@gmail.com>
2026-07-03 10:45:54 +0530

    Basis Intro About Nodejs

---
+++ Node-Learning/00-What_Is_Node/index.js
@@ -0,0 +1,36 @@
+//What_Is_Node
+
+//Definition
+
+// Node.js is a JavaScript Runtime Environment that allows JavaScript to run outside the browser.
+
+
+ /*Node.js ka Birth
+
+2009 me ek developer ne socha...
+JavaScript itni fast language hai.
+To browser ke bahar bhi chalni chahiye.
+Usne Chrome ke JavaScript Engine V8 ko use kiya aur uske around ek runtime banaya.
+
+Us runtime ka naam rakha gaya: Node-js*/
+
+
+/*Internal Working (Basic)
+
+
+Your JavaScript Code
+          │
+          ▼
+      Node.js Runtime
+          │
+          ▼
+       V8 Engine// chrome
+       fairfox // spidermonkey
+       safari  // apple
+          │
+          ▼
+     Machine Code
+          │
+          ▼
+     Operating System
+*/
