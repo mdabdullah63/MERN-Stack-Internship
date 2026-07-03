42518a6f68e08f3664298bf87025da467a94fd66
MD Abdullah <mdabdullahrah67@gmail.com>
2026-07-03 11:52:27 +0530

    Hello world in Node

---
+++ Node-Learning/01-Hello_World/index.js
@@ -0,0 +1,37 @@
+console.log("Hello World");
+
+/* node index
+Hello World
+
+node need to write js because node only run js file so it defult select js extension
+
+node index.js
+Hello World
+
+DOM and Window are no avaialbe in Node V8 engine
+
+
+NPM init is used to intinalization of NPM
+ Make template
+ make package.json
+ {
+  "name": "01-hello_world",
+  "version": "1.0.0",
+  "description": "",
+  "main": "index.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1",
+
+        "start": "node index.js"
+        we can call directly by  start
+        npm start 
+  },
+  "author": "Md Abdullah",
+  "license": "ISC",
+  "type": "commonjs"
+}
+
+this is configure file every thing is done in this file
+*/
+
+
