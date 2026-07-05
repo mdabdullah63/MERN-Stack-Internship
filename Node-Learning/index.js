8c143ec3160519a9f9facbefdf7b67378804560f
MD Abdullah <mdabdullahrah67@gmail.com>
2026-07-05 12:46:54 +0530

    Versioning and there working

---
+++ Node-Learning/index.js
@@ -0,0 +1,33 @@
+/*
+SemVer (Semantic Versioning)
+
+        1 . 5 . 3
+        │   │   │
+        │   │   └── PATCH → Bug fixes
+        │   └────── MINOR → New features
+        └────────── MAJOR → Breaking changes
+
+Format:
+MAJOR.MINOR.PATCH
+
+Package Version Symbols
+
+1. "5.1.0"   → Sirf isi version ko install karega.
+2. ^5.1.0    → Minor + Patch update allow, Major nahi.
+3. ~5.1.0    → Sirf Patch update allow.
+4. >5.1.0    → Isse bada koi bhi version.
+5. >=5.1.0   → Ye version ya isse bada koi bhi version.
+6. <5.1.0    → Isse chhota koi bhi version.
+7. <=5.1.0   → Ye version ya isse chhota koi bhi version.
+8. *         → Koi bhi version.
+9. 5.x       → Sirf Major version 5 ke andar koi bhi version.
+10. @latest  → Latest stable version install karega.
+11. @5.1.0   → Specific version install karega.
+*/
+
+/*
+Meaning of Versioning
+
+Versioning ka matlab hai API ke multiple versions banana, taki naye changes karne par purane applications break na hon. Isse backward compatibility maintain rehti hai aur old aur new clients dono ek saath API use kar sakte hain.
+
+*/
